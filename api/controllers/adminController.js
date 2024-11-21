c0dfc163faa242437c010eb94dd82b6ff9fca9eb
sel-tarun <tarun.vuppala@aceec.ac.in>
2024-11-21 20:38:28 +0530

    Added admin logout and register endpoint

--- api/controllers/adminController.js
+++ api/controllers/adminController.js
@@ -37,3 +37,44 @@ export const adminLogin = async (req, res, next) => {
         httpError(next, error, req, 500);
     }
 }
+
+export const adminLogout = async (req, res, next) => {
+    try {
+        res.clearCookie('token');
+        httpResponse(req, res, 200, responseMessage.LOGOUT_SUCCESS);
+    } catch (error) {
+        httpError(next, error, req, 500);
+    }
+}
+
+export const adminResgistration = async (req, res, next) => {
+    try {
+        const { username, password, email, role } = req.body;
+        if (!username || !password || !email) {
+            httpError(next, new Error(responseMessage.MISSING_FIELDS), req, 400);
+            return;
+        }
+
+        const exists = await Admin.findOne({ email });
+        if (exists) {
+            httpError(next, new Error(responseMessage.RESOURCE_ALREADY_EXISTS('admin')), req, 400);
+            return;
+        }
+
+        const admin = await Admin.create({
+            username,
+            password,
+            email,
+            role
+        });
+        
+        httpResponse(req, res, 200, responseMessage.REGISTRATION_SUCCESS, {
+            admin: {
+                username: admin.username,
+            },
+            role: admin.role,
+        });
+    } catch (error) {
+        httpError(next, error, req, 500);
+    }
+}
--- api/router/v1/adminRouter.js
+++ api/router/v1/adminRouter.js
@@ -1,12 +1,16 @@
 import express from 'express';
 
-import { adminLogin } from '../../controllers/adminController.js';
+import { adminLogin, adminLogout, adminResgistration } from '../../controllers/adminController.js';
 import adminAuth from '../../../middleware/adminAuth.js';
 import httpResponse from '../../../utils/httpResponse.js';
 
 const router = express.Router();
 
 router.post('/login', adminLogin);
+
+router.post('/logout', adminLogout);
+
+router.post('/register', adminResgistration);
 
 router.get('/', adminAuth, (req, res) => {
     httpResponse(req, res, 200, 'success', { message: "Welcome!!" });
