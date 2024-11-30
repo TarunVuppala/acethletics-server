d86b61505b749b89c6bf17479076b0131b576389
sel-tarun <tarun.vuppala@aceec.ac.in>
2024-11-30 19:12:26 +0530

    cors update

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: ['http://localhost:3000', 'https://acetheletics.aceec.ac.in'],
+        origin: ['http://localhost:3000', 'https://acetheletics.aceec.ac.in', 'https://acethletics-server.onrender.com'],
         methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT'],
     },
 });
